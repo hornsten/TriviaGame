2765a0a43a36a8b9db54bd2a8cf6e661e4adf024
Jessica Hornsten <hornsten@users.noreply.github.com>
2016-10-31 10:49:22 -0500

    Trying to get timers working correctly

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,8 +2,11 @@ $(document).ready(function() {
 
     var time = 30;
     var counter;
+    isCorrect = false;
+    isWrong = false;
+    var id;
 
-    // $('#stop').on('click', stop);
+    //$('.stop').on('click', stop(time));
 
     // $('#start').on('click', run);
 
@@ -19,11 +22,11 @@ $(document).ready(function() {
         $('#timer').html('<h2>' + "Time Remaining: " + time + '</h2>');
 
         if (time === 0) {
-            stop(timer);
+            stop(counter);
             $('#timer').html('<h2>Your time is up!</h2');
             return;
-        } else {
 
+        } else {
             write();
         }
     };
@@ -40,46 +43,69 @@ $(document).ready(function() {
         id: 0,
         wrongChoices: ["Bea Arthur", "Wilma Flintstone", "Weakest Link Lady"],
         answer: "LaVerne V",
-        imgsrc: '#'
+        imgsrc: '0'
     }, {
 
         question: "Who's your daddy?",
         id: 1,
         wrongChoices: ["Don Draper", "Fred Rogers", "Dr. Phil"],
         answer: "Richard P",
-        imgsrc: '#'
+        imgsrc: '1'
     }, {
         question: "Who is your mama?",
         id: 2,
-        wrongChoices: ["Bea Arthur", "Wilma Flintstone", "Weakest Link Lady"],
-        answer: "LaVerne V",
-        imgsrc: '#'
+        wrongChoices: ["Mama Cass", "Hoosier Mama", "Mamma Mia"],
+        answer: "Sue P",
+        imgsrc: '2'
     }, {
         question: "Who is your sis?",
         id: 3,
-        wrongChoices: ["Elvira", "Bearded Lady", "Sasha Fierce"],
-        answer: "Mimi Marmora",
-        imgsrc: '#'
+        wrongChoices: ["Twisted Sister", "Sister Sledge", "Sister Christian"],
+        answer: "Shelley K",
+        imgsrc: '3'
     }];
 
     function choices(wrongs) {
         var choiceButtons = wrongs.map(function(errs) {
-            return '<button type="button" class="btn btn-default">' + errs + '</button>';
+            return '<button type="button" class="btn btn-default stop">' + errs + '</button>';
         }).join("");
         return '<div class="btn-group-vertical">' + choiceButtons + '</div>';
     }
 
     function write() {
-        for (let i = 0; i < triviaQuestions.length; i++) {
-            setTimeout(function timer() {
-                $('#question').html(triviaQuestions[i].question);
-                $('#choices').html('<button type="button" class="btn btn-default">' + triviaQuestions[i].answer +
-                    choices(triviaQuestions[i].wrongChoices) + '</button>');
-            }, i * 1000);
 
+        for (let i = 0; i < triviaQuestions.length; i++) {
+
+            setTimeout(function timer() {
+                // $('#question').html(triviaQuestions[i].question);
+
+                // $('#choices').html('<button type="button" class="btn btn-default">' + triviaQuestions[i].answer +
+                //     choices(triviaQuestions[i].wrongChoices) + '</button>');
+                console.log(triviaQuestions[i].id);
+                id = triviaQuestions[i].id;
+
+            }, i * 30000);
+
+            console.log("id is" + id);
 
         }
+        $('#question').html(triviaQuestions[id].question);
     }
+
+
+    $('.btn-default').on('click', function() {
+        alert("clicked!");
+        if (isCorrect === true) {
+            return;
+
+        } else {
+
+            $('#question').html("The correct answer is " + triviaQuestions[this].answer + ".");
+            stop();
+
+        }
+
+    });
 
     run();
 });
