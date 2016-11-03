5e75479053640375a94b9ae72e1fe16e171c0786
Jessica Hornsten <hornsten@users.noreply.github.com>
2016-11-02 20:05:52 -0500

    Mixes up the choices so that the correct answer is in a different position each time.  Uses .html to detect which choice is clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,26 +23,24 @@ $(document).ready(function() {
             $('#start').hide();
 
 
-            $('.incorrect').on('click', function() {
+            $('.btn-danger').on('click', function() {
 
-                console.log(this);
-                stop();
-                showAnswer();
-                $('#timer').html('<h2>I\'m afraid not...</h2>');
-                setTimeout(run, 1000 * 5);
-                incorrect++;
-
+                if ($(this).html() !== triviaQuestions[index].answer) {
+                    stop();
+                    showAnswer();
+                    $('#timer').html('<h2>I\'m afraid not...</h2>');
+                    setTimeout(run, 1000 * 5);
+                    incorrect++;
+                } else {
+                    stop();
+                    showAnswer();
+                    $('#timer').html('<h2>You got it!</h2>');
+                    setTimeout(run, 1000 * 5);
+                    correct++;
+                }
             });
 
-            $('.correct').on('click', function() {
-                console.log(this);
-                stop();
-                showAnswer();
-                $('#timer').html('<h2>You got it!</h2>');
-                setTimeout(run, 1000 * 5);
-                correct++;
 
-            });
 
         }
 
@@ -99,37 +97,40 @@ $(document).ready(function() {
 
         question: 'Who said, "Live from New York, it\'s Saturday Night!" on the very first episode?',
         id: 0,
-        wrongChoices: ['Dan Aykroyd', 'Jane Curtin', 'John Belushi'],
+        wrongChoices: ['Dan Aykroyd', 'Jane Curtin', 'Chevy Chase', 'John Belushi'],
         answer: 'Chevy Chase',
         imgsrc: 'assets/images/chevy.gif'
     }, {
 
         question: 'Which 90s "Bad Boy of SNL" was born in Madison, WI?',
         id: 1,
-        wrongChoices: ['Chris Rock', 'Adam Sandler', 'David Spade'],
+        wrongChoices: ['Chris Farley', 'Chris Rock', 'Adam Sandler', 'David Spade'],
         answer: 'Chris Farley',
         imgsrc: 'assets/images/farley.gif'
     }, {
         question: 'What is the least successful film based on an SNL character?',
         id: 2,
-        wrongChoices: ['"MacGruber"', '"The Ladies Man"', '"Stuart Saves His Family"'],
+        wrongChoices: ['"MacGruber"', '"The Ladies Man"', '"Stuart Saves His Family"', '"It\'s Pat"'],
         answer: '"It\'s Pat"',
         imgsrc: 'assets/images/pat.gif'
     }, {
         question: 'Which SNL writer quit in a rage, then showed up to work on Monday as if nothing happened?',
         id: 3,
-        wrongChoices: ['Tina Fey', 'Conan O\'Brien', 'John Mulaney'],
+        wrongChoices: ['Tina Fey', 'Larry David', 'Conan O\'Brien', 'John Mulaney'],
         answer: 'Larry David',
         imgsrc: 'assets/images/larry.gif'
 
 
     }];
 
+    var number = triviaQuestions[0].answer;
+    console.log(number);
+    alert(triviaQuestions[0].wrongChoices[number]);
 
 
-    function choices(wrongs, ans) {
+    function choices(wrongs) {
         var choiceButtons = wrongs.map(function(errs) {
-            return '<button type="button" class="btn btn-lg btn-danger incorrect">' + errs + '</button>';
+            return '<button type="button" class="btn btn-lg btn-danger">' + errs + '</button>';
         }).join('');
 
         return choiceButtons;
@@ -138,11 +139,12 @@ $(document).ready(function() {
 
 
     function write(index) {
+
         var wrongies = choices(triviaQuestions[index].wrongChoices);
-        var righty = '<button type="button" class="btn btn-lg btn-danger correct">' + triviaQuestions[index].answer + '</button>';
+        // var righty = '<button type="button" class="btn btn-lg btn-danger">' + triviaQuestions[index].answer + '</button>';
 
         $('#question').html(triviaQuestions[index].question);
-        $('#choices').html(righty + wrongies);
+        $('#choices').html(wrongies);
 
     }
 
